827891a6d722dc5b9d899e4910b120c917270325
Maimouna <mcamara2@terpmail.umd.edu>
2019-11-19 11:14:17 -0500

    completeds
    2 paragraphs from about.js completed

--- build/src/components/Documentation.js
+++ build/src/components/Documentation.js
@@ -10,12 +10,11 @@ class Documentation extends React.Component{
 
         <h3 className="header">This section will explain how to use the application and possible features it contains.</h3>
         <p className="paragraph">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-        officia deserunt mollit anim id est laborum.
+        All of our information will be presented in a graph format. The graphs will display 
+        changes in crime trends in a way that will help with human visual processing and 
+        understanding. Some features will include displaying the change of our data after 
+        we implement changing variables or conditions that impact our data in a positive or 
+        negative way. 
         </p>
 
         {/* Insert Image here */}
