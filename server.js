0b948e34428c913684df8931f2f32e2a54959d17
fchambergo <franky4908@gmail.com>
2019-12-04 15:15:28 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "build")));
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'build')))
-;}
+  app.use(express.static(path.join(__dirname, 'build')));
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
