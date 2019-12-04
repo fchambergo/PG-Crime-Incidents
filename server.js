76a6407fbc88286e21b035d77a36b468399f9c18
fchambergo <franky4908@gmail.com>
2019-12-04 08:33:38 -0500

    server.js changes

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production"){
-  app.use(express.static("/build"));
-}
+app.use(express.static("/build"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -30,6 +28,10 @@ app.get('/api', (req, res) => {
         res.redirect('/error');
       });
   });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
