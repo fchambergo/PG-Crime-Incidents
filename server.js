05ad872f964b3ce3a6ba472c61b4e7230fd94c11
fchambergo <franky4908@gmail.com>
2019-12-04 08:10:53 -0500

    Deployment changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production"){
-  app.use(express.static(_dirname));
+  app.use(express.static("/build"));
 }
 
 app.use(express.urlencoded({ extended: true }));
