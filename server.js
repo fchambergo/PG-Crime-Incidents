70db847890d019c2ae63dca7795c61b5a138ffe2
fchambergo <franky4908@gmail.com>
2019-12-04 09:19:33 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static("/build"));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
