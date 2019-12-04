8234d7e5f7aa5f2f5cc6b2d2c00b3e9b780aff5c
fchambergo <franky4908@gmail.com>
2019-12-04 16:22:38 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const fetch = require('node-fetch');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "build")));
-
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'build')));
-}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -35,7 +31,7 @@ app.get('/api', (req, res) => {
   });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname = '/build/index.html'));
+  res.sendFile(path.join(__dirname = '/build/public/index.html'));
 })
 
 
