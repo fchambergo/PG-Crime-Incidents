e4c682f77028ea5989e76971f5dee38078e3676c
fchambergo <franky4908@gmail.com>
2019-12-04 16:28:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(__dirname + "/build"));
+
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static(path.join(__dirname, 'build')));
+// }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -30,9 +34,9 @@ app.get('/api', (req, res) => {
       });
   });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname = '/build/public/index.html'));
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname = '/build/index.html'));
+// })
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
