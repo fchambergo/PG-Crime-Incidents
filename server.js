eb236b060424941bc470d17d62565cd3a538387e
fchambergo <franky4908@gmail.com>
2019-12-04 19:21:58 -0500

    server.js update

--- server.js
+++ server.js
@@ -31,9 +31,13 @@ app.get('/api', (req, res) => {
 
 app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-})
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  })
+}
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
