d3e5b7fe666f35b4f6cc2b8c208a349ff086c414
fchambergo <franky4908@gmail.com>
2019-12-08 00:46:47 -0500

    Changed web application logo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
