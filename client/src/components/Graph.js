3dbac0f1843ba0c55a4457732bc0b69539c3affa
ACLParker <Hyramaeus@hotmail.com>
2020-06-23 23:21:27 -0400

    Reimplementation of Colorset
    The final version of the project from 2019 seems to have been altered at some point, perhaps due to a commit from an earlier branch. This fixes the color scheme to once again using a gradient set. Perhaps look into why the backend loading is not as efficient as it used to be.

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -84,19 +84,19 @@ class Graph extends React.Component {
             }
         });
 
-    //     CanvasJS.addColorSet("sunsetShades",
-    //     [//colorSet Array
+         CanvasJS.addColorSet("sunsetShades",
+         [//colorSet Array
 
-    //     "#003f5c",
-    //     "#2f4b7c",
-    //     "#665191",
-    //     "#a05195",
-    //     "#d45087",
-    //     "#f95d6a",
-    //     "#ff7c43",
-    //     "#ffa600",
-    //     "#ffd100"
-    // ]);
+         "#003f5c",
+         "#2f4b7c",
+         "#665191",
+         "#a05195",
+         "#d45087",
+         "#f95d6a",
+         "#ff7c43",
+         "#ffa600",
+         "#ffd100"
+     ]);
 
         /* Canvas JS Graph variables */
 		const options = {
